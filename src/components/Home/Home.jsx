14679d2afde8e4bd1e6cc293a363b6733f11f1aa
Your Name <you@example.com>
2025-09-09 17:56:37 +0300

    Refactor Home component and update welcome message for clarity

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Home.css";
 import RecipeCard from "../RecipeCard/RecipeCard";
 import CardGrid from "../CardGrid/CardGrid";
 import "../Components.css";
